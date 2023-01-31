10a8a3a41b0798c355a9bfcbf41ff132659b435a
Tatianna Martinez <tatianna.martinez71@gmail.com>
2023-01-31 11:58:46 -0700

    Finished Chapter 03-12 Practice

--- javascript-essential-training-2832077-main/Practice/03_12/script.js
+++ javascript-essential-training-2832077-main/Practice/03_12/script.js
@@ -6,3 +6,60 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+import Backpack from "./Backpack.js";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "purple",
+  4,
+  35,
+  35,
+  false
+);
+console.log("The everydayPack object:", everydayPack);
+console.log("The pocketNum value:", everydayPack.pocketNum);
+
+class Animals {
+  constructor(
+    name,
+    species,
+    color,
+    collarOn,
+    furType,
+    fedBreakfast,
+    fedDinner
+  ) {
+    this.name = name;
+    this.species = species;
+    this.color = color;
+    this.collarOn = collarOn;
+    this.furType = furType;
+    this.fedMeal = {
+      breakfast: fedBreakfast,
+      dinner: fedDinner,
+    };
+  }
+
+  toggleCollar(status) {
+    this.collarOn = status;
+  }
+
+  updateMeal(fedBreakfast, fedDinner) {
+    this.fedMeal.breakfast = fedBreakfast;
+    this.fedMeal.dinner = fedDinner;
+  }
+}
+
+const cade = new Animals(
+  "Butthead",
+  "Cat",
+  "Black",
+  true,
+  "Floofy",
+  true,
+  false
+);
+
+console.log("The stupid Cade is:", cade)
+console.log("He has been fed breakfast:", cade.fedMeal.breakfast)
